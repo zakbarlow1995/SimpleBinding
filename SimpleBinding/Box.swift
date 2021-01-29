4fc54dc824258e8181ec4fe6651ffb01e0af12f7
zakbarlow1995 <zak@stormideas.com>
2021-01-29 09:42:04 +0000

    Added Box<T>, a wrapper class for dynamic binding + example code showing MVVM view-viewModel binding in action

---
+++ SimpleBinding/Box.swift
@@ -0,0 +1,37 @@
+//
+//  Box.swift
+//  SimpleBinding
+//
+//  Created by Zak Barlow on 28/01/2021.
+//
+
+import Foundation
+
+/// A wrapper class for dynamic binding
+class Box<T> {
+    
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    /// Setup binding and fire with current Box\<T\>.value
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+    
+    /// Setup binding and await change to Box\<T\>.value before executing closure
+    func bindAwait(listener: Listener?) {
+        self.listener = listener
+    }
+    
+}
--- SimpleBinding/ViewController.swift
+++ SimpleBinding/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var goToNextVCButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func goToNextVCButtonPressed(_ sender: UIButton) {
+        print("Button pressed")
+        
+        let newVC = ColorVC()
+        newVC.configure(with: ColorViewModel())
+        navigationController?.pushViewController(newVC, animated: true)
+    }
 }
 
+class ColorVC: UIViewController {
+    
+    var viewModel: ColorViewModel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewModel?.getNewColorFromApi()
+    }
+    
+    
+    func configure(with viewModel: ColorViewModel) {
+        
+        self.viewModel = viewModel
+        
+        viewModel.color.bind { [weak self] color in
+            DispatchQueue.main.async {
+                guard let self = self else { return }
+                self.view.backgroundColor = color
+            }
+        }
+    }
+}
+
+class ColorViewModel {
+    
+    var color = Box(UIColor.red)
+    
+    func getNewColorFromApi() {
+        //Goes to your api to fetch a color
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
+            self.color.value = [.green, .purple, .blue, .orange, .yellow, .cyan, .magenta, .brown].randomElement() ?? .red
+        }
+    }
+    
+}
